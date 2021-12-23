66e145b4b23943044f2fa45da8c42e31e6c7ee0c
Bhargav-Kumar-Murala <bhargav.murala@apntbs.com>
2021-12-23 14:39:05 +0530

    Genre display fix

--- bhargav_kumar_murala/lib/View/home_screen.dart
+++ bhargav_kumar_murala/lib/View/home_screen.dart
@@ -89,12 +89,37 @@ class HomeScreen extends StatelessWidget {
                                                     fontWeight:
                                                         FontWeight.bold),
                                               ),
-                                              const Text(
-                                                'state.search![index].genre!',
-                                                style: TextStyle(
-                                                    fontSize: 10,
-                                                    fontWeight:
-                                                        FontWeight.w300),
+                                              Expanded(
+                                                child: ListView.separated(
+                                                    scrollDirection:
+                                                        Axis.horizontal,
+                                                    shrinkWrap: true,
+                                                    itemBuilder: (context, i) {
+                                                      return Text(
+                                                        _genre(state
+                                                            .search![index]
+                                                            .genreIds![i]),
+                                                        style: const TextStyle(
+                                                            fontSize: 10,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .w300),
+                                                      );
+                                                    },
+                                                    separatorBuilder:
+                                                        (context, index) =>
+                                                            const Text(
+                                                              ' | ',
+                                                              style: TextStyle(
+                                                                  fontSize: 10,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w300),
+                                                            ),
+                                                    itemCount: state
+                                                        .search![index]
+                                                        .genreIds!
+                                                        .length),
                                               ),
                                               const SizedBox(height: 7),
                                               Container(
@@ -118,7 +143,8 @@ class HomeScreen extends StatelessWidget {
                                                       color: Colors.white,
                                                       fontSize: 12),
                                                 ),
-                                              )
+                                              ),
+                                              const Spacer(flex: 4),
                                             ],
                                           ),
                                         ),
@@ -177,6 +203,67 @@ class HomeScreen extends StatelessWidget {
           state.search[index].posterPath);
     } else {
       return const AssetImage("assets/logo.jpg");
+    }
+  }
+
+  _genre(value) {
+    switch (value) {
+      case 28:
+        return 'Action';
+      case 12:
+        return 'Adventure';
+      case 16:
+        return 'Animation';
+      case 35:
+        return 'Comedy';
+      case 80:
+        return 'Crime';
+      case 99:
+        return 'Documentary';
+      case 18:
+        return 'Drama';
+      case 10751:
+        return 'Family';
+      case 14:
+        return 'Fantasy';
+      case 36:
+        return 'History';
+      case 27:
+        return 'Horror';
+      case 10402:
+        return 'Music';
+      case 9648:
+        return 'Mystery';
+      case 10749:
+        return 'Romance';
+      case 878:
+        return 'Science Fiction';
+      case 10770:
+        return 'TV Movie ';
+      case 53:
+        return 'Thriller';
+      case 10752:
+        return 'War';
+      case 37:
+        return 'Western';
+      case 10759:
+        return 'Action & Adventure';
+      case 10762:
+        return 'Kids';
+      case 10763:
+        return 'News';
+      case 10764:
+        return 'Reality';
+      case 10765:
+        return 'Sci-Fi & Fantasy';
+      case 10766:
+        return 'Soap';
+      case 10767:
+        return 'Talk';
+      case 10768:
+        return 'War & Politics';
+      default:
+        return 'NA';
     }
   }
 }
